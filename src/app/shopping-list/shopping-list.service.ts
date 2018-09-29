7f08fdf981a769c14dae8e0e92b770f0a2963bde
VasylSluzhala <mu.1.chested.09@gmail.com>
2018-09-29 15:35:01 +0300

    test 2

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -24,6 +24,5 @@ export class ShoppingListService{
     addIngredients(ingredients: Ingredient[]){
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.emit(this.ingredients.slice());
-        
     }
 }
